137ce8b5ccdad8ceebd8035a86548cfba585d98c
Erick Gabriel <erickgabrie56@gmail.com>
2025-08-22 15:46:41 -0300

    22-08-2025 15:46

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -8,3 +8,14 @@ function menuShow(){
         document.querySelector('.icon').src = "images/menu/close_white_36dp.svg";
     }
 }
+
+const navLinks = document.querySelectorAll('.mobile-menu .nav-link');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        let menuMobile = document.querySelector('.mobile-menu');
+        if (menuMobile.classList.contains('open')) {
+            menuMobile.classList.remove('open');
+            document.querySelector('.icon').src = "images/menu/menu_white_36dp.svg";
+        }
+    });
+});
